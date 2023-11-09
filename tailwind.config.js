1124ace3ca5778eae1aef9b16add94f9253460f8
AmarnathCKR <ckramarnath@gmail.com>
2023-11-09 18:23:48 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode : "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
+
